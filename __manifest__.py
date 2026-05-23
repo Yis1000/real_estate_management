fe34ea64665025bc34c7e91371322b088ecae8f0
Yisus <jesusnuevoper@gmail.com>
2026-05-23 20:36:58 +0200

    Initial commit: App description and basic module structure

---
+++ __manifest__.py
@@ -0,0 +1,26 @@
+{
+    'name': 'Real Estate Management',
+    'version': '1.0',
+    'summary': 'Manage real estate properties, contracts, maintenance, and visits',
+    'description': """
+Real Estate Ultimate Manager
+============================
+A complete module for real estate management.
+    """,
+    'category': 'Sales/Real Estate',
+    'author': 'Higa Solutions',
+    'website': 'https://higa.group',
+    'license': 'OPL-1',
+    'depends': ['base', 'mail', 'calendar'],
+    'data': [
+        # Security
+        # Data
+        # Views
+        # Reports
+    ],
+    'demo': [],
+    'images': ['static/description/screenshot_kanban_inmuebles.png'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
